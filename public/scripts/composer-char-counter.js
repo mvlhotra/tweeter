26497823fb56aa1646f31258636fdba0f0a9d30d
Nik Malhotra <nmal@me.com>
2019-02-19 23:09:57 +0000

    added a tweet

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+  const text = document.querySelector('.new-tweet textarea');
+  text.addEventListener('input', function () {
+    const len = this.value.length;
+    let count = ($(this).siblings(".counter"));
+    count[0].innerText = 140 - len;
+    if (count[0].innerText < 0) {
+      $(count).addClass("negative");
+    } else {
+      $(count).removeClass("negative");
+    }
+  });
+});
