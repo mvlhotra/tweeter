71c6095f8fd23543febbd7ee44c41057901ff22b
Nik Malhotra <nmal@me.com>
2019-02-20 16:21:55 +0000

    dynamic addition of tweets added

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,3 +4,97 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+$(document).ready(function () {
+
+  const dateDiff = function calculateDateDifference(date1, date2) {
+    dt1 = new Date(date1);
+    dt2 = new Date(date2);
+    return -1 * Math.floor((Date.UTC(dt2.getFullYear(), dt2.getMonth(), dt2.getDate()) - Date.UTC(dt1.getFullYear(), dt1.getMonth(), dt1.getDate())) / (1000 * 60 * 60 * 24));
+
+  }
+  const data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": {
+          "small": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
+          "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
+          "large": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+        },
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
+    },
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": {
+          "small": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
+          "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
+          "large": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
+        },
+        "handle": "@rd"
+      },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    },
+    {
+      "user": {
+        "name": "Johann von Goethe",
+        "avatars": {
+          "small": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
+          "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
+          "large": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
+        },
+        "handle": "@johann49"
+      },
+      "content": {
+        "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
+      },
+      "created_at": 1461113796368
+    }
+  ];
+
+  const crTweet = function createTweetElement(tweet) {
+
+    let $tweet = `      
+    <article class="tweet">
+      <header>
+        <img src="${tweet.user.avatars.small}">
+        <h1>${tweet.user.name}</h1>
+        <p class="handle">${tweet.user.handle}</p>
+      </header>
+      <div class="content">
+        <p class="tweet-content">${tweet.content.text}</p>
+      </div>
+      <footer>
+        <p class="posted">${dateDiff(Date.now(), tweet.created_at)} days ago</p>
+        <div class="icons">
+          <i class="fas fa-flag"></i>
+          <i class="fas fa-retweet"></i>
+          <i class="fas fa-heart"></i>
+        </div>
+      </footer>
+    </article>
+  `
+    return $tweet;
+
+  };
+
+  function renderTweets(tweets) {
+    tweets.forEach(tweet => {
+      let content = crTweet(tweet);
+      $('.feed').append(content);
+    });
+    return;
+  }
+  renderTweets(data);
+
+});
+
+ // to add it to the page so we can make sure it's got all the right elements, classes, etc.
